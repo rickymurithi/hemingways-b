ef580b09ca44e60af1bd0473d165ed10aac9d19e
kimuit117 <kemoikiuita@gmail.com>
2022-11-09 10:59:34 +0300

    saves progress

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
-  
+  # Add your routes here
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
+  get "/feedbacks" do
+    feedback = Feedback.all
+    feedback.to_json
+  end
 
 end
+
+
+
+
+
+

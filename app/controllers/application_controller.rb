0b4dd81dd61764fc441d9f4c690b0d4b8ef53ae2
jeffoty <odhiambo47@gmail.com>
2022-11-09 23:44:40 +0300

    added more routes to be used by our sinatra app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,53 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/feedbacks" do
+    feedback = Feedback.all
+    feedback.to_json
+  end
+
+  get "/bookings" do
+    booking = Booking.all
+    booking.to_json
+  end
+
+  get "/feedbacks/:id" do
+    feedback = Feedback.find(params[:id])
+    feedback.to_json
+  end
+
+  get "/bookings/:id" do
+    booking = Booking.find(params[:id])
+    booking.to_json
+  end
+
+  post "/feedbacks" do
+    feedback = Feedback.create(rating: params[:rating], comment: params[:comment], name: params[:name], photo_url: params[:photo_url])
+    feedback.to_json
+  end
+
+  post "/bookings" do
+    booking = Booking.create(name: params[:name], email: params[:email], phone_number: params[:phone_number],suite: params[:suite])
+    booking.to_json
+  end
+
+  patch "/feedbacks/:id" do
+    booking =Booking.find(params[:id])
+    booking.update(comment: params[:comment])
+    booking.to_json
+  end
+
+  delete "/bookings/:id" do
+    booking = Booking.find(params[:id])
+    booking.destroy
+    booking.to_json
+  end
+
+  delete "/feedbacks/:id" do
+    feedback = Feedback.find(params[:id])
+    feedback.destroy
+    feedback.to_json
+  end 
+
+
 end

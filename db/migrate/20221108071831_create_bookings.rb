4912620cbccc4302f752b30e405d50074357ad14
jeffoty <odhiambo47@gmail.com>
2022-11-11 10:49:53 +0300

    made changes in the booking table

--- db/migrate/20221108071831_create_bookings.rb
+++ db/migrate/20221108071831_create_bookings.rb
@@ -1,7 +1,7 @@
 class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
-      t.string :name
+      t.string :Name
       t.string :email
       t.string :phone_number
       t.string :suite 
